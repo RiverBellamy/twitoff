09c011fd324e046286b0c8f5c9fc867e17606d6d
RiverOfRationality <frank.bellamy@gmail.com>
2021-01-28 12:51:37 -0500

    installying cython for deployment - heroku

---
+++ twitoff/models.py
@@ -0,0 +1,27 @@
+"""SQLAlchemy models and utility functions for TwitOff"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+DB = SQLAlchemy()
+
+
+class User(DB.Model):  # User Table
+    """Twitter Users corresponding to Tweets"""
+    id = DB.Column(DB.BigInteger, primary_key=True)  # id column
+    name = DB.Column(DB.String, nullable=False)  # name column
+
+    def __repr__(self):
+        return "<User: {}>".format(self.name)
+
+
+class Tweet(DB.Model):
+    """Tweets corresponding to Users"""
+    id = DB.Column(DB.BigInteger, primary_key=True)  # id column
+    text = DB.Column(DB.Unicode(300))  # tweet text column - allows for emojis
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey(
+        "user.id"), nullable=False)  # user_id column (corresponding user)
+    user = DB.relationship("User",
+                           backref=DB.backref("tweets", lazy=True))  # creates user link between tweets
+
+    def __repr__(self):
+        return "<Tweet: {}>".format(self.text)
